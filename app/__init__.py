ecd4296bb86c0de7080da37d6108511152e79d2a
sashana <sashana.ramsay@mymona.uwi.edu>
2018-03-23 15:28:31 +0000

    added some css

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 UPLOAD_FOLDER = "./app/static/uploads"
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "mykEy"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:mypassword@localhost/project1"#
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://dtlbflvzbigjzg:5e04db81f5638ef194bf53d9c669cadede2ab4a70d34934376bb51610cc72f61@ec2-54-83-23-91.compute-1.amazonaws.com:5432/d7nirlmqm7p7kp"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
--- app/forms.py
+++ app/forms.py
@@ -7,8 +7,8 @@ from wtforms.widgets import TextArea
 class ProfileForm(FlaskForm):
     firstname = StringField('First Name', validators=[InputRequired()])
     lastname = StringField('Last Name', validators=[InputRequired()])
-    gender = SelectField(label='Gender', choices=[("Male", "Male"), ("Female", "Female")])
+    gender = SelectField(label='Gender',choices=[("Male", "Male"), ("Female", "Female")])
     email = StringField('Email', validators=[InputRequired()])
     location = StringField('Location', validators=[InputRequired()])
     biography = StringField('Biography', widget=TextArea(),validators=[InputRequired()])
-    upload = FileField('Pofile Picture',validators=[FileRequired('File required for upload'),FileAllowed(['jpg', 'png'], 'Images only!')])
+    upload = FileField('Profile Picture',validators=[FileRequired('File required for upload'),FileAllowed(['jpg', 'png'], 'Images only!')])
